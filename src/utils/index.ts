8238c9683c8aa9b3feed2d8a304298dc4ac390ef
Aki Amane <80670461+akippnn@users.noreply.github.com>
2023-03-21 06:31:55 +0800

    Changes: - Retry in case text generation fails (when OpenAI returns unexpected errors) - Send a message that an error occurred if the bot still can't generate a reply - Send message in chunks, in case the text is over 2000 characters long

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export { default as getCurrentTime } from "./getCurrentTime";
 export { default as getTimeElapsedString } from "./getTimeElapsedString";
+export { default as retry } from "./retry";
 export * from "./stringManipulation";
